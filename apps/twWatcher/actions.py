1c789507ba0fcc77c82d59ac7a1fbbe63d6096ff
Aspect13 <ad13box@gmail.com>
2021-12-31 05:39:11 +0300

    adding chromium args

--- apps/twWatcher/actions.py
+++ apps/twWatcher/actions.py
@@ -18,6 +18,8 @@ def get_driver():
     chrome_options.add_argument('--headless')
     # chrome_options.add_argument('--remote-debugging-port=9222')
     # chrome_options.add_argument('--remote-debugging-address=0.0.0.0')
+    chrome_options.add_argument('--no-sandbox')
+    chrome_options.add_argument('--disable-dev-shm-usage')
     chrome_options.add_argument('--mute-audio')
 
     driver = webdriver.Chrome(options=chrome_options)
